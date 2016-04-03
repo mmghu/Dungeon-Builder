9384d4a2751a4d8a09ef5fbe6bc1a81822e995b9
mmghuneim <muaeim@gmail.com>
2016-04-02 21:22:20 -0400

    Quick commit
    Need to redo graphics again

--- src/com/dungeonbuilder/graphics/MapExplorerPanel.java
+++ src/com/dungeonbuilder/graphics/MapExplorerPanel.java
@@ -1,26 +1,89 @@
 package com.dungeonbuilder.graphics;
 
 import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JPanel;
 
 import com.dungeonbuilder.map.Map;
+import com.dungeonbuilder.units.Character;
 
-public class MapExplorerPanel extends JPanel{
+public class MapExplorerPanel extends JPanel implements MouseListener{
 
 	// member variables
 	private static final long serialVersionUID = -2513127929733972381L;
 	private Map map;
 	
+	private boolean clicked;
+	private Character clickedCharacter;
+	
 	// constructor
 	public MapExplorerPanel(Map m){
 		map = m;
+		addMouseListener(this);
 	}
 	
 	// methods
 	@Override
 	public void paint(Graphics g){
 		map.drawMap(g);
+		map.drawUnits(g);
+		
+		for(int i = 0; i < map.getCharacters().size(); i++){
+			if(map.getCharacters().get(i).isUpdated()){
+				map.getCharacters().get(i).moveUnit(g, map.getxOrigin(), map.getyOrigin(), map.getCharacters().get(i).getNextX(), map.getCharacters().get(i).getNextY(), map.getMultiplier(), map);
+			}
+		}
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// convert click to coorindate
+		int xCoord = map.getCoordinate(e.getX(), true);
+		int yCoord = map.getCoordinate(e.getY(), false);
+		
+		if(!clicked){
+			if(map.getTile(xCoord, yCoord).hasCharacter()){
+				clicked = true;
+				clickedCharacter = map.getTile(xCoord, yCoord).getCharacter();
+				System.out.println("clicked character");
+			}
+		}
+		if(clicked){
+			if(!map.getTile(xCoord, yCoord).hasEnemy() && !map.getTile(xCoord, yCoord).hasCharacter()){
+				clickedCharacter.setUpdated(true);
+				clickedCharacter.setNextX(xCoord);
+				clickedCharacter.setNextY(yCoord);
+				
+				clicked = false;
+				System.out.println("moved character");
+			}
+		}
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 } // end class
--- src/com/dungeonbuilder/map/Map.java
+++ src/com/dungeonbuilder/map/Map.java
@@ -4,6 +4,10 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Toolkit;
+import java.util.ArrayList;
+
+import com.dungeonbuilder.units.Enemy;
+import com.dungeonbuilder.units.Character;
 
 public class Map {
 
@@ -11,10 +15,19 @@ public class Map {
 	private String name;
 	private Tile[][] tiles;
 	
+	private int xOrigin, yOrigin;
+	private int multiplier;
+	
+	private ArrayList<Enemy> enemies;
+	private ArrayList<Character> characters;
+	
 	// constructor
 	public Map(String n, Tile[][] t){
 		name = n;
 		tiles = t;
+		
+		enemies = new ArrayList<Enemy>();
+		characters = new ArrayList<Character>();
 	}
 	
 	// accessors
@@ -26,9 +39,20 @@ public class Map {
 	public int getWidth(){return tiles.length;}
 	public int getHeight(){return tiles[0].length;}
 	
+	public int getxOrigin(){return xOrigin;}
+	public int getyOrigin(){return yOrigin;}
+	
+	public int getMultiplier(){return multiplier;}
+	
+	public ArrayList<Enemy> getEnemies(){return enemies;}
+	public ArrayList<Character> getCharacters(){return characters;}
+	
 	// mutators
 	public void setName(String n){name = n;}
 	public void setTile(int x, int y, Tile t){tiles[x][y] = t;}
+	
+	public void addEnemy(Enemy e){enemies.add(e);}
+	public void addCharacter(Character c){characters.add(c);}
 	
 	// methods
 	public void drawMap(Graphics g){
@@ -40,43 +64,58 @@ public class Map {
 		
 		/* temporary - fix later - centers map on the screen */
 		
-		int multiplier = 30;
-		int xshift = (width - (getWidth() * multiplier))/2;
-		int yshift = (height - (getHeight() * multiplier))/2;
+		multiplier = 30;
+		xOrigin = (width - (getWidth() * multiplier))/2;
+		yOrigin = (height - (getHeight() * multiplier))/2;
 		
 		// draw the background
 		g.setColor(Color.WHITE);
-		g.fillRect(xshift, yshift, getWidth() * multiplier, getHeight() * multiplier);
+		g.fillRect(xOrigin, yOrigin, getWidth() * multiplier, getHeight() * multiplier);
 		
 		// draw the map
 		g.setColor(Color.BLACK);
 		
-		for(int x = xshift; x <= ((getWidth()) * multiplier) + xshift; x += multiplier){
-			g.drawLine(x, yshift, x, (getHeight() * multiplier) + yshift);
+		for(int x = xOrigin; x <= ((getWidth()) * multiplier) + xOrigin; x += multiplier){
+			g.drawLine(x, yOrigin, x, (getHeight() * multiplier) + yOrigin);
 		}
 		
-		for(int y = yshift; y <= ((getHeight()) * multiplier) + yshift; y += multiplier){
-			g.drawLine(xshift, y, (getWidth() * multiplier) + xshift, y);
+		for(int y = yOrigin; y <= ((getHeight()) * multiplier) + yOrigin; y += multiplier){
+			g.drawLine(xOrigin, y, (getWidth() * multiplier) + xOrigin, y);
 		}
-		
+	}
+	
+	public void drawUnits(Graphics g){
 		// draw the enemies
 		for(int x = 0; x < getWidth(); x++){
 			for(int y = 0; y < getHeight(); y++){
 				if(tiles[x][y].hasEnemy()){
-					tiles[x][y].getEnemy().drawUnit(g, xshift, yshift, x, y, multiplier);
+					tiles[x][y].getEnemy().drawUnit(g, xOrigin, yOrigin, x, y, multiplier);
 				}
 			}
 		}
-		
+				
 		// draw the characters
 		for(int x = 0; x < getWidth(); x++){
 			for(int y = 0; y < getHeight(); y++){
 				if(tiles[x][y].hasCharacter()){
-					tiles[x][y].getCharacter().drawUnit(g, xshift, yshift, x, y, multiplier);
+					tiles[x][y].getCharacter().drawUnit(g, xOrigin, yOrigin, x, y, multiplier);
 				}
 			}
 		}
+	}
+	
+	// convert pixel to coordinate
+	public int getCoordinate(int p, boolean xShift){
+		int diff;
 		
+		if(xShift){
+			diff = p - xOrigin;
+		}
+		else{
+			diff = p - yOrigin;
+		}
+
+		return (diff / multiplier);
 	}
 	
 } // end class
